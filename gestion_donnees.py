8168e85df5b897500e4c04d08864fd4875859ecd
Hugo-Freitas <55703354+Hugo-Freitas@users.noreply.github.com>
2022-12-06 23:26:01 -0500

    correction

--- gestion_donnees.py
+++ gestion_donnees.py
@@ -6,19 +6,53 @@
 # Hugo FREITAS COSTINHA (22 140 773)
 ####
 
+
+from sklearn.preprocessing import LabelEncoder # Gérer les noms des cibles
 from sklearn.model_selection import train_test_split
-import pandas as pd
 
 
-class GestionDonnees:
-    def generer_donnes(self) :
-        train = pd.read_csv("train.csv")
-        X = train["species"]
-        y = train.drop["species"]
 
-        print("Generation des données de test...")
+class GestionDonnees :
+    def __init__(self, bd) :
+        #self.donnees_base = donnees_base
+        self.bd = bd
+        
+    def lecture_donnees(self, base_d) :
+        """
+        Paramètres
+        ----------
+        base_d :  Numpy array
+                  Matrice lu du fichier base avec l'ensemble de données d'entraînement.
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+        Returns
+        -------
+        f_types :  list
+                    Liste avec les noms des types de feuilles.
+        x_base :   Numpy array
+                    Contient uniquement les données d'entraînement.
+        t_base :   Numpy array
+                    Contient uniquement les cibles (chiffres) pour l'entraînement.
 
-        return X_train, y_train, X_test, y_test
+        """
+        # Lire les types des feuilles du fichier train
+        encoder = LabelEncoder().fit(base_d.species)
+        f_types = list(encoder.classes_)
+        t_base = encoder.transform(base_d.species)
+        
+        # Séparer id de l'ensemble de données
+        x_base_df = base_d.drop(['id', 'species'], axis= 1 )
+        x_base = x_base_df.to_numpy()
+        
+        return f_types, x_base, t_base
+    
+    def sep_donnees(self, x_data,t_data) :
+        
+        # Separer les données pour entraînement et test
+        x_tr, x_te, t_tr, t_te = train_test_split(x_data, t_data, test_size = 0.25, \
+                                                  random_state = 7)
+                
+        return x_tr, x_te, t_tr, t_te
+            
+
+
 

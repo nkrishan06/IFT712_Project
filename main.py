549683acf87f8b1e0799974f640c367728c00edc
nkrishan06 <navas.6krishan@gmail.com>
2022-12-12 22:27:48 -0500

    ajout plt et sn

--- main.py
+++ main.py
@@ -9,6 +9,8 @@
 import pandas as pd
 import os
 import time
+
+from matplotlib import pyplot as plt, image as mpimg
 from sklearn.metrics import precision_recall_fscore_support as metriques
 from sklearn.metrics import accuracy_score as accu
 from tabulate import tabulate
@@ -24,8 +26,6 @@ import regression_logistique
 
 # Ignorer les warnings
 from warnings import simplefilter
-
-
 
 simplefilter(action='ignore')
 
@@ -55,6 +55,7 @@ elif algorithme == 'Arbre_de_decisions':
 elif algorithme == 'analyse_discriminant_lineaire':
     classif = analyse_discriminant_lineaire.Analyse_Discriminant_lineaire()
 
+
 def main():
     # Caractéristique de la base de données
     d_base.head()
@@ -78,30 +79,31 @@ def main():
     # Séparer les données et leur cibles
     g_donnees = gd.GestionDonnees(d_base)
     [types, X, t] = g_donnees.lecture_donnees(d_base)
-    
+
     # Séparer les données pour test et train
     x_tr, x_ts, t_tr, t_ts = g_donnees.sep_donnees(X, t)
-        
+
     # Entraînement
-    debut_e = time.time() # Heure de debut pour mesurer le temps d'entraînement
+    debut_e = time.time()  # Heure de debut pour mesurer le temps d'entraînement
     classif.entrainement(x_tr, t_tr, cherche_hyp)
-    fin_e = time.time() # Heure de fin pour mesurer le temps d'entraînement
-    print('Fin de l\'entrainement. Réalisé en %.2f secondes.'% (fin_e - debut_e),'\n')
-    
+    fin_e = time.time()  # Heure de fin pour mesurer le temps d'entraînement
+    print('Fin de l\'entrainement. Réalisé en %.2f secondes.' % (fin_e - debut_e), '\n')
+
     # Prédictions pour les ensembles d'entraînement et de test
     predict_tr = classif.prediction(x_tr)
     predict_ts = classif.prediction(x_ts)
-    
+
     # Métriques pour évaluer l'entraînement et test
     prs_tr, rec_tr, fbeta_tr, _ = metriques(t_tr, predict_tr, average='macro')
     prs_ts, rec_ts, fbeta_ts, _ = metriques(t_ts, predict_ts, average='macro')
     acc_tr = accu(t_tr, predict_tr)
     acc_ts = accu(t_ts, predict_ts)
-    tab_perform = [['Accuracy', acc_tr, acc_ts],['Précision', prs_tr, prs_ts],\
-                   ['Rappel', rec_tr, rec_ts],['F-Beta', fbeta_tr, fbeta_ts]]
+    tab_perform = [['Accuracy', acc_tr, acc_ts], ['Précision', prs_tr, prs_ts], \
+                   ['Rappel', rec_tr, rec_ts], ['F-Beta', fbeta_tr, fbeta_ts]]
     print(tabulate(tab_perform, headers=['Metrique', 'Train', 'Test'], floatfmt='.4f'))
-   
+
     return tab_perform
+
 
 if __name__ == "__main__":
     main()

3cf05f34f41cf1b2321c510f249759155c0b23bd
nkrishan06 <navas.6krishan@gmail.com>
2022-12-12 22:23:26 -0500

    Ajout dataset & caracteristique

--- main.py
+++ main.py
@@ -19,42 +19,62 @@ import SVM
 import k_proches_voisins
 import gaussian_bayesienne
 import arbre_decision
-import regression_logistique
 import analyse_discriminant_lineaire
-
+import regression_logistique
 
 # Ignorer les warnings
 from warnings import simplefilter
+
+
+
 simplefilter(action='ignore')
 
 # Lire la base de données
-d_base = pd.read_csv(os.getcwd() + '\\train.csv')
+d_base = pd.read_csv(os.getcwd() + '\\dataset\\train.csv')
 
 algorithme = 'Arbre_de_decisions'
 cherche_hyp = True
 
-#Importer l'algorithme correspondant
+# Importer l'algorithme correspondant
 
 if algorithme == 'Regression_logistique':
     classif = regression_logistique.Regression_logistique()
-    
+
 elif algorithme == 'SVM':
     classif = SVM.SupportVectorMachine()
-    
-elif algorithme == 'K-proches_voisins': 
+
+elif algorithme == 'K-proches_voisins':
     classif = k_proches_voisins.KProchesVoisins()
-    
-elif algorithme == 'Gaussian_Bayesienne': 
+
+elif algorithme == 'Gaussian_Bayesienne':
     classif = gaussian_bayesienne.GaussianBayes()
-    
-elif algorithme == 'Arbre_de_decisions': 
+
+elif algorithme == 'Arbre_de_decisions':
     classif = arbre_decision.ArbreDecision()
-    
-elif algorithme == 'analyse_discriminant_lineaire': 
+
+elif algorithme == 'analyse_discriminant_lineaire':
     classif = analyse_discriminant_lineaire.Analyse_Discriminant_lineaire()
 
 def main():
-    
+    # Caractéristique de la base de données
+    d_base.head()
+    d_base.describe()
+    d_base.duplicated().sum()
+    d_base.isnull().sum().sum()
+    d_base['species'].nunique()
+    d_base['species'].unique()
+
+    # images
+    id = 1
+    plt.imshow(mpimg.imread(os.getcwd() + "\\dataset\\images\\" + str(id) + ".jpg"), cmap="gray")
+    plt.title("Image " + str(id) + ": " + d_base[d_base["id"] == id].values[:, 1][0]);
+    plt.show()
+
+    # Correlation
+    df = d_base.copy()
+    correlation = df.drop("id", axis=1,
+                          inplace=False).corr()  # On enlève l'id car pas pertinent et on réalise la correlation
+
     # Séparer les données et leur cibles
     g_donnees = gd.GestionDonnees(d_base)
     [types, X, t] = g_donnees.lecture_donnees(d_base)
